7812844d71d2608f36f91bb3e1cbca2075e02044
Viraj Amarasinghe <virajamara@gmail.com>
2023-02-01 02:46:32 +0530

    postgres connection

--- php/Expertizing/public/index.php
+++ php/Expertizing/public/index.php
@@ -2,7 +2,9 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+phpinfo();
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
